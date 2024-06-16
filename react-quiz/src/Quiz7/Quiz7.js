70bc30a660e0f246645984c90524457c2e8bea94
mi-k-7 <rudd12@bu.ac.kr>
2024-06-16 17:41:29 +0900

    setTimeout 초 변경

--- react-quiz/src/Quiz7/Quiz7.js
+++ react-quiz/src/Quiz7/Quiz7.js
@@ -51,7 +51,7 @@ function Quiz7() {
     useEffect(() => {
         const timeOut = setTimeout(() => {
             setVisible(false);
-        }, 70000);
+        }, 7000);
         return () => clearTimeout(timeOut);
     }, []);
 
@@ -59,7 +59,7 @@ function Quiz7() {
     useEffect(() => {
         const timeOut = setTimeout(() => {
             setInvisible(true);
-        }, 70000);
+        }, 7000);
         return () => clearTimeout(timeOut);
     }, []);
     
